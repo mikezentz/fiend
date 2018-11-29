a1d73b5529092bde9d7ae17f0f9eccdd6af64e9f
mike <mike@pop-os.localdomain>
2018-11-28 20:37:50 -0800

    added heroku functionality

--- fiend/settings.py
+++ fiend/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
